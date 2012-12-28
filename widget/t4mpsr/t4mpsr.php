7b4ff48d0d9c03ebed48a60875c6097d051739f4
Fisher <fisher@fisher.hu>
2012-12-28 11:05:25 +0100

    display balances added to the widget

--- widget/t4mpsr/t4mpsr.php
+++ widget/t4mpsr/t4mpsr.php
@@ -8,7 +8,6 @@
  */
 
 
-add_action('widgets_init', 't4mpsr_widget' );
 add_action('show_user_profile', 't4mpsr_extra_user_profile_fields' );
 add_action('edit_user_profile', 't4mpsr_extra_user_profile_fields' );
 add_action('personal_options_update', 't4mpsr_save_extra_user_profile_fields' );
@@ -70,14 +69,20 @@ function t4mpsr_widget() {
 	if( is_wp_error( $response ) ) {
 	    echo 'Something went wrong!';
 	} else {
-	   print_r($response['body']);
-	   
+		$t4mpsr_data = explode("\n", $response['body']);
+		$tb=$t4mpsr_data[0];
+		$gb=$t4mpsr_data[1];
+		$gs=$t4mpsr_data[2];
+		print('<p><strong>A te egyenleged:</strong> ' . number_format_i18n($tb) . '</p>');
+		print('<p><strong>A teljes egyenleg: </strong> ' . number_format_i18n($gb) . '</p>');
+		print('<p><strong>Tartalék: </strong> ' . number_format_i18n($gs) . '</p>');
+		
 	}
 }
 
 function add_t4mpsr_widget() {
 	wp_add_dashboard_widget('t4mpsr_widget','hu3 egyenleg', 't4mpsr_widget');
-	}
+}
 
 
 
